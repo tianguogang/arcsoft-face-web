3dcd17b21a83881194dd5d4cfaf8e61c925a0d9b
tianguogang <1369729295@qq.com>
2023-04-18 00:18:12 +0800

    打卡成功

--- src/views/face/AddFaceInfo.vue
+++ src/views/face/AddFaceInfo.vue
@@ -1,165 +1,63 @@
 <template>
-  <div class="camera_outer">
-    <video id="videoCamera" :width="videoWidth" :height="videoHeight" autoplay></video>
-    <canvas style="display:none;" id="canvasCamera" :width="videoWidth" :height="videoHeight" ></canvas>
-
-    <div v-if="imgSrc" class="img_bg_camera">
-      <img :src="imgSrc" alt="" class="tx_img">
-    </div>
-    <button @click="getCompetence()">打开摄像头</button>
-    <button @click="stopNavigator()">关闭摄像头</button>
-    　　　　　<button @click="setImage()">拍照</button>
-
-
-
+  <div>
+    <video ref="video" autoplay></video>
+    <button @click="takePicture">Take Picture</button>
+    <canvas ref="canvas" style="display:none;"></canvas>
+    <img :src="photo" v-if="photo"/>
   </div>
 </template>
+
 <script>
+import {ref, onMounted} from 'vue';
+import _axios, {dataUrl} from "@/http/request";
+
 export default {
-  data () {
-    return {
-      videoWidth: 3000,
-      videoHeight: 300,
-      imgSrc: '',
-      thisCancas: null,
-      thisContext: null,
-      thisVideo: null,
-    }
-  },
-  methods: {
-// 调用权限（打开摄像头功能）
-    getCompetence () {
-      var _this = this
-      this.thisCancas = document.getElementById('canvasCamera')
-      this.thisContext = this.thisCancas.getContext('2d')
-      this.thisVideo = document.getElementById('videoCamera')
-      // 旧版本浏览器可能根本不支持mediaDevices，我们首先设置一个空对象
-      if (navigator.mediaDevices === undefined) {
-        navigator.mediaDevices = {}
+  setup() {
+    const video = ref(null);
+    const canvas = ref(null);
+    const photo = ref(null);
+
+    const startCamera = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({video: true});
+        video.value.srcObject = stream;
+      } catch (err) {
+        console.error('Error accessing camera:', err);
       }
-      // 一些浏览器实现了部分mediaDevices，我们不能只分配一个对象
-      // 使用getUserMedia，因为它会覆盖现有的属性。
-      // 这里，如果缺少getUserMedia属性，就添加它。
-      if (navigator.mediaDevices.getUserMedia === undefined) {
-        navigator.mediaDevices.getUserMedia = function (constraints) {
-          // 首先获取现存的getUserMedia(如果存在)
-          var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.getUserMedia
-          // 有些浏览器不支持，会返回错误信息
-          // 保持接口一致
-          if (!getUserMedia) {
-            return Promise.reject(new Error('getUserMedia is not implemented in this browser'))
-          }
-          // 否则，使用Promise将调用包装到旧的navigator.getUserMedia
-          return new Promise(function (resolve, reject) {
-            getUserMedia.call(navigator, constraints, resolve, reject)
-          })
-        }
-      }
-      var constraints = { audio: false, video: { width: this.videoWidth, height: this.videoHeight, transform: 'scaleX(-1)' } }
-      navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
-        // 旧的浏览器可能没有srcObject
-        if ('srcObject' in _this.thisVideo) {
-          _this.thisVideo.srcObject = stream
-        } else {
-          // 避免在新的浏览器中使用它，因为它正在被弃用。
-          _this.thisVideo.src = window.URL.createObjectURL(stream)
-        }
-        _this.thisVideo.onloadedmetadata = function (e) {
-          _this.thisVideo.play()
-        }
-      }).catch(err => {
-        console.log(err)
+    };
+
+    const takePicture = () => {
+      canvas.value.width = video.value.videoWidth;
+      canvas.value.height = video.value.videoHeight;
+      canvas.value.getContext('2d').drawImage(video.value, 0, 0, canvas.value.width, canvas.value.height);
+      const imageData = canvas.value.toDataURL('image/png');
+      photo.value = imageData;
+      console.log(imageData)
+
+
+
+      _axios.post(dataUrl + "/fact-info/check ", {imageData:imageData}).then((res) => {
+        console.log(res)
       })
-    },
-//  绘制图片（拍照功能）
+    };
 
-    setImage () {
-      var _this = this
-      // 点击，canvas画图
-      _this.thisContext.drawImage(_this.thisVideo, 0, 0, _this.videoWidth, _this.videoHeight)
-      // 获取图片base64链接
-      var image = this.thisCancas.toDataURL('image/png')
-      _this.imgSrc = image
-      this.$emit('refreshDataList', this.imgSrc)
-    },
-// base64转文件
+    const detectFace = () => {
+      // 使用人脸识别API检测图像中是否有人脸
+      // 如果有人脸，自动拍照
+      // 可以使用第三方库，例如face-api.js
+    };
 
-    dataURLtoFile (dataurl, filename) {
-      var arr = dataurl.split(',')
-      var mime = arr[0].match(/:(.*?);/)[1]
-      var bstr = atob(arr[1])
-      var n = bstr.length
-      var u8arr = new Uint8Array(n)
-      while (n--) {
-        u8arr[n] = bstr.charCodeAt(n)
-      }
-      return new File([u8arr], filename, { type: mime })
-    },
-// 关闭摄像头
+    onMounted(() => {
+      startCamera();
+      setInterval(detectFace, 1000);
+    });
 
-    stopNavigator () {
-      this.thisVideo.srcObject.getTracks()[0].stop()
-    }
+    return {
+      video,
+      canvas,
+      photo,
+      takePicture,
+    };
   },
-
-}
-
+};
 </script>
-<style lang="less" scoped>
-.camera_outer{
-  position: relative;
-  overflow: hidden;
-  //background: url("") no-repeat center;
-  background-size: 100%;
-  video,canvas,.tx_img{
-    -moz-transform:scaleX(-1);
-    -webkit-transform:scaleX(-1);
-    -o-transform:scaleX(-1);
-    transform:scaleX(-1);
-  }
-  .btn_camera{
-    position: absolute;
-    bottom: 4px;
-    left: 0;
-    right: 0;
-    height: 50px;
-    background-color: rgba(0,0,0,0.3);
-    line-height: 50px;
-    text-align: center;
-    color: #ffffff;
-  }
-  .bg_r_img{
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    top: 0;
-  }
-  .img_bg_camera{
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    top: 0;
-    img{
-      width: 100%;
-      height: 100%;
-    }
-    .img_btn_camera{
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      right: 0;
-      height: 50px;
-      line-height: 50px;
-      text-align: center;
-      background-color: rgba(0,0,0,0.3);
-      color: #ffffff;
-      .loding_img{
-        width: 50px;
-        height: 50px;
-      }
-    }
-  }
-}
-</style>
--- src/views/face/CheckFaceInfo.vue
+++ src/views/face/CheckFaceInfo.vue
@@ -1,13 +1,79 @@
 <template>
-人脸比对
+  <div style="display:flex;justify-content: center">
+   <div >
+     <video style="width: 90vh"   ref="video" autoplay></video><br/>
+  <div style="display:flex;justify-content: center">    <a-radio-button  @click="takePicture">身份鉴定</a-radio-button> </div>
+   </div>
+   <div style="display: flex;">
+     <canvas ref="canvas" style="display:none;"></canvas>
+<!--     <img style="margin-left: 30px" :src="photo" v-if="photo"/>-->
+     <a-card hoverable style="width: 90vh;margin-left: 30px" v-if="photo">
+       <template #cover>
+         <img alt="example"   :src="photo"   />
+       </template>
+       <a-card-meta :title="userInfo.message"  >
+       </a-card-meta>
+       <hr/>
+        <p v-if="userInfo.data">年龄： {{userInfo.data.age}} &nbsp;&nbsp;&nbsp;&nbsp 性别：{{userInfo.data.gender=1?"男":"女"}}  <span v-if="userInfo.data">&nbsp;&nbsp;&nbsp;&nbsp 活体检测： {{userInfo.data.liveness=1?'活体':'图片'}}</span></p>
+
+
+     </a-card>
+   </div>
+  </div>
 </template>
 
 <script>
+import {ref, onMounted} from 'vue';
+import _axios, {dataUrl} from "@/http/request";
+
 export default {
-  name: "CheckFaceInfo"
-}
+  setup() {
+    const video = ref(null);
+    const canvas = ref(null);
+    const photo = ref(null);
+    const userInfo = ref({});
+
+    const startCamera = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({video: true});
+        video.value.srcObject = stream;
+      } catch (err) {
+        console.error('Error accessing camera:', err);
+      }
+    };
+
+    const takePicture = () => {
+      canvas.value.width = video.value.videoWidth;
+      canvas.value.height = video.value.videoHeight;
+      canvas.value.getContext('2d').drawImage(video.value, 0, 0, canvas.value.width, canvas.value.height);
+      const imageData = canvas.value.toDataURL('image/png');
+      photo.value = imageData;
+
+
+
+      _axios.post(dataUrl + "/fact-info/check ", {imageData:imageData}).then((res) => {
+        console.log(res)
+       userInfo.value=res.data
+      })
+    };
+
+    const detectFace = () => {
+      // 使用人脸识别API检测图像中是否有人脸
+      // 如果有人脸，自动拍照
+      // 可以使用第三方库，例如face-api.js
+    };
+
+    onMounted(() => {
+      startCamera();
+      setInterval(detectFace, 1000);
+    });
+
+    return {
+      video,
+      canvas,
+      photo,userInfo,
+      takePicture,
+    };
+  },
+};
 </script>
-
-<style scoped>
-
-</style>
